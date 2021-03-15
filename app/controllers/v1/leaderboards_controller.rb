a7fb4d08759e5bc81b7006e54e142daf7a682d88
Ryan Waits <ryan.waits@gmail.com>
2021-03-15 12:32:32 -0500

    add page size param to leaderboard

--- app/controllers/v1/leaderboards_controller.rb
+++ app/controllers/v1/leaderboards_controller.rb
@@ -4,7 +4,8 @@ class V1::LeaderboardsController < ApplicationController
   def index
     @leaderboard_name = "#{params[:type].upcase}_LEADERBOARD".constantize
     page = params[:page] || 1
-    @users = @leaderboard_name.leaders(page.to_i, page_size: 5, with_member_data: true)
+    page_size = params[:page_size] || 5
+    @users = @leaderboard_name.leaders(page.to_i, page_size: page_size.to_i, with_member_data: true)
     respond_with @users
   end
 end
